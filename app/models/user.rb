59f6f4672c190740581f47d33af0643fcfccd687
zhangcaiyan <zhangcaiyan@cn.jobs>
2014-04-13 10:54:13 +0800

    devise 账户系统

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :lockable, :authentication_keys => [:username]
+
+  validates_presence_of :password, :password_confirmation
+  validates :email, format: {with: RubyRegex::Email}, uniqueness: true, allow_blank: true
+  validates :username, uniqueness: true
+  validates :password, confirmation: true, length: {minimum: 6}
+
+
+end
