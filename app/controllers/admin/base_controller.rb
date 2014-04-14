d340c629c9e8f9d997ab96a18a1e65f9066b0a2b
zhangcaiyan <zhangcaiyan@cn.jobs>
2014-04-14 15:56:12 +0800

    添加管理员的校验

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,8 +1,15 @@
 class Admin::BaseController < ApplicationController
 
   # layout "admin"
+  before_action :authenticate_admin!
 
 
+  protected
 
+  def authenticate_admin!
+    if !user_signed_in? || !current_user.admin?
+        redirect_to home_path
+    end
+  end
 
 end
