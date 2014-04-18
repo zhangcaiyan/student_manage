f16461ff0568754df7567eb204e3ec677dc94e29
zhangcaiyan <zhangcaiyanbeyond@gmail.com>
2014-04-19 06:46:50 +0800

    后台

--- app/controllers/xuegongchu/home_controller.rb
+++ app/controllers/xuegongchu/home_controller.rb
@@ -1,6 +1,5 @@
 class Xuegongchu::HomeController < Xuegongchu::BaseController
   def index
-
-
+    @current_date_reward_applications = RewardApplication.search(created_at_gte: Date.current, state_eq: "已上报").result
   end
 end
