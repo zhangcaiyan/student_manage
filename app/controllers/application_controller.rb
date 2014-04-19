328f3401e0b086b54a21d8111c8add54ee305902
zhangcaiyan <zhangcaiyanbeyond@gmail.com>
2014-04-20 00:17:04 +0800

    系统前台添加header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_action :authenticate_user!
+
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   layout :layout_by_resource
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
   end
 
   def layout_by_resource
-    if params[:controller] == "devise/registrations" && params[:action] == "edit"
+    if params[:controller] == "devise/registrations" && params[:action] == "edit" || params[:action] == "update"
       "admin"
     else
       "application"
