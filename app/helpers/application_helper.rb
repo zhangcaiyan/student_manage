963d9d8ee250268a6b4c3aa83ecb975965b5d4e2
zhangcaiyan <zhangcaiyanbeyond@gmail.com>
2014-04-20 02:11:45 +0800

    fixbug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,23 @@ module ApplicationHelper
       html.html_safe
     end
   end
+
+  def title(title)
+    content_for :title do
+      title + " - "
+    end
+  end
+
+  def js(*js)
+    content_for :script do
+      javascript_include_tag *js
+    end
+  end
+
+  def css(*css)
+    content_for :head do
+      stylesheet_link_tag *css
+    end
+  end
+
 end
